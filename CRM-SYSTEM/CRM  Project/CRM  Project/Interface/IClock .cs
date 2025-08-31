7ff68cd1a6a2af12232e6b2eaaeb4d06044f02ad
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-31 21:49:42 +0300

    Add files via upload

---
+++ CRM-SYSTEM/CRM  Project/CRM  Project/Interface/IClock .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM__Project.Interface
+{
+    internal interface IClock
+    {
+        public static DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+}
